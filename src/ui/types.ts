6cd69fb90b1b540c0c6e0c476649c50931e3c56c
keshan <keshan011@live.com>
2020-05-07 12:29:19 +0530

    types added

---
+++ src/ui/types.ts
@@ -0,0 +1,21 @@
+import {
+  FlexDirectionProps,
+  DisplayProps,
+  SpaceProps,
+  BorderProps,
+  BorderColorProps,
+  BordersProps,
+  BackgroundColorProps,
+  FontSizeProps,
+  LineHeightProps
+} from "styled-system";
+
+export type StyleProps = FlexDirectionProps &
+  SpaceProps &
+  DisplayProps &
+  BorderProps &
+  BorderColorProps &
+  BordersProps &
+  BackgroundColorProps &
+  FontSizeProps &
+  LineHeightProps;
