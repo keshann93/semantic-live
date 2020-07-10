80d7660c06907e57b62337def1f26c6eaea2a53a
keshan <keshan011@live.com>
2020-07-10 11:34:11 +0530

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,86 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = env => ({
-  entry: "./src/ui/index.tsx",
-  mode: env.production ? "production" : "development",
+  entry: './src/ui/index.tsx',
+  mode: env.production ? 'production' : 'development',
   output: {
-    path: path.resolve(__dirname, "./out/ui"),
-    filename: "ui.bundle.js",
-    publicPath: "http://localhost:9000/"
+    path: path.resolve(__dirname, './out/ui'),
+    filename: 'ui.bundle.js',
+    publicPath: 'http://localhost:9000/',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "../dist/ui"),
+    contentBase: path.resolve(__dirname, '../dist/ui'),
     hot: true,
     port: 9000,
     disableHostCheck: true,
     headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
+      'Access-Control-Allow-Origin': '*',
+    },
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /(node_modules)/,
-        loader: "babel-loader"
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015', 'stage-2'],
+        },
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]"
-            }
-          }
-        ]
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "PaintBox",
-      template: require("html-webpack-template"),
-      appMountId: "root",
-      inject: false
+      title: 'PaintBox',
+      template: require('html-webpack-template'),
+      appMountId: 'root',
+      inject: false,
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      PRODUCTION: env.production === true
-    })
-  ]
+      PRODUCTION: env.production === true,
+    }),
+  ],
 });
